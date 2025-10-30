31f2eaa7e219ddb2c58683375fb0034f757549a1
Shunsuke MIYAKE <github@helicalworks.com>
2025-10-30 16:57:22 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import UnifiedProfileComposer from './components/UnifiedProfileComposer_v4';
+import UnifiedProfileComposer from './components/UnifiedProfileComposer_v3';
 
 function App() {
   return (
